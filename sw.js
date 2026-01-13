18c6e84738ef860c48b5ac793a44bbeffc090a7a
mei1996kang <angel6225a@gmail.com>
2026-01-13 14:43:07 +0800

    re

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'japan-trip-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  'https://cdn.tailwindcss.com'
+const CACHE_NAME = 'jp-trip-v2';
+const assets = [
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@500;700&family=Noto+Sans+TC:wght@300;400;700&display=swap'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
